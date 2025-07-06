53a4189f61bd11897c70c34d9a96f86ea7fe99b4
toktobek-01 <toktobek.d@gmail.com>
2025-07-05 20:31:46 -0700

    new pro

--- src/OriginalInReversed.java
+++ src/OriginalInReversed.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 public class OriginalInReversed {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter your name: ");
-        String input = scanner.nextLine();
 
+        String input = scanner.nextLine().toUpperCase();
         char[] original = input.toCharArray();
         char[] reversed = new char[original.length];
 
         for(int i = 0; i < original.length; i++) {
-            reversed[i] = original[original.length - 1 - i];
+            reversed[i] = original [original.length - 1 - i];
         }
 
-        System.out.println("Reversed name from new array: ");
+        System.out.println("Reversed name from a new array: ");
         for(char c : reversed) {
-            System.out.print(c);
+            System.out.println(c);
         }
 
+        String reversName = new String(reversed);
+        System.out.println("Reverseed name as a String is: " + reversName);
     }
 }
