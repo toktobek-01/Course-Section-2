ea9ef50c1a7b17d65e9b4f82d2d165c92fc916e2
toktobek-01 <toktobek.d@gmail.com>
2025-07-05 19:05:44 -0700

    new pro

---
+++ src/MyNameArray.java
@@ -0,0 +1,12 @@
+public class MyNameArray {
+    public static void main(String[] args) {
+        String[] name = {"J", "O", "H", "N"};
+        System.out.println(name);
+
+        for(int i = 0; i < name.length; i++) {
+            System.out.print(name[i]);
+        }
+
+
+    }
+}
