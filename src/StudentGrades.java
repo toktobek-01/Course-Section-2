564a268fd7c3d7c60c309bf7d1953daf8793cc1f
toktobek-01 <toktobek.d@gmail.com>
2025-07-06 17:14:09 -0700

    new pro

--- src/StudentGrades.java
+++ src/StudentGrades.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class StudentGrades {
@@ -14,26 +15,31 @@ public class StudentGrades {
 
         String name = "";
 
-        while(!name.equalsIgnoreCase("exit")) {
-            System.out.println("Enter the name of a student (or type 'exit' to quit): ");
+        while (!name.equalsIgnoreCase("exit")) {
+            System.out.print("Enter the name of a student (or type 'list' to see all, 'exit' to quit): ");
             name = sc.nextLine();
 
-            if(name.equalsIgnoreCase("exit")) {
+            if (name.equalsIgnoreCase("exit")) {
                 System.out.println("Goodbye!!!");
-            } else if ( grades.containsKey(name)) {
+            } else if (name.equalsIgnoreCase("list")) {
+                System.out.println("📋 All students and their grades:");
+                for (Map.Entry<String, Integer> entry : grades.entrySet()) {
+                    System.out.println(entry.getKey() + " → " + entry.getValue());
+                }
+            } else if (grades.containsKey(name)) {
                 System.out.println("The student's grade is: " + grades.get(name));
             } else {
-                System.out.println("Students not found. Do you want to add this student? (yes/no)");
-                String asnwer = sc.nextLine();
+                System.out.println("Student not found. Do you want to add this student? (yes/no)");
+                String answer = sc.nextLine();
 
-                if(asnwer.equalsIgnoreCase("yes")) {
-                    System.out.println("Enter the student's grade: ");
+                if (answer.equalsIgnoreCase("yes")) {
+                    System.out.print("Enter the student's grade: ");
                     int newGrade = sc.nextInt();
-                    sc.nextLine();
+                    sc.nextLine(); // clear buffer
                     grades.put(name, newGrade);
                     System.out.println(name + " has been added with grade: " + newGrade);
                 }
             }
-            }
         }
     }
+}
