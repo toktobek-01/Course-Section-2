c8ed5486ad7cddd8b9af579d559784cc07af2d0d
toktobek-01 <toktobek.d@gmail.com>
2025-07-16 13:13:20 -0500

    new pro

---
+++ src/LocalDate.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+public class LocalDate{
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+    }
+}
+
+
+
